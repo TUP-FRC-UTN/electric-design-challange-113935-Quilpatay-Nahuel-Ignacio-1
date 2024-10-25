e3be5b6399249e685a2fede26a46dbb25e1106b5
Nahuel Ignacio Quilpatay - 113935 <113935@tecnicatura.frc.utn.edu.ar>
2024-10-24 23:11:26 -0300

    initial

--- src/app/budget-list/budget-list.component.ts
+++ src/app/budget-list/budget-list.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
+import { BudgetService } from '../services/budget.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-budget-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './budget-list.component.html',
-  styleUrl: './budget-list.component.css',
 })
 export class BudgetListComponent {
-  /* ADDITIONAL DOCS:
-    - https://angular.dev/guide/components/lifecycle#
-    - https://angular.dev/guide/http/making-requests#http-observables
-    - https://angular.dev/guide/http/setup#providing-httpclient-through-dependency-injection
-    - https://angular.dev/guide/http/making-requests#setting-request-headers
-    - https://angular.dev/guide/http/making-requests#handling-request-failure
-    - https://angular.dev/guide/http/making-requests#best-practices (async pipe)
-    - https://angular.dev/guide/testing/components-scenarios#example-17 (async pipe)
-  */
+  
+  constructor(private budgetService: BudgetService) {}
+  
+  budgets$ = this.budgetService.getBudgets();
+  
+  ngOnInit(): void {}
 }
--- src/app/budget-view/budget-view.component.ts
+++ src/app/budget-view/budget-view.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { map, Observable, switchMap } from 'rxjs';
+import { BudgetService } from '../services/budget.service';
+import { GroupedModules } from '../models/budget';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-budget-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './budget-view.component.html',
-  styleUrl: './budget-view.component.css',
 })
 export class BudgetViewComponent {
-  // ADDITIONAL DOCS: same as BudgetListComponent
+  budgetData$: Observable<any>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private budgetService: BudgetService
+  ) {}
+
+  ngOnInit(): void {
+    this.budgetData$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        const id = params.get('id')!;
+        return this.budgetService.getBudget(id).pipe(
+          map((budget) => ({
+            budget,
+            requiredBoxes: this.budgetService.calculateRequiredBoxes(
+              budget.modules
+            ),
+            groupedModules: this.budgetService.groupModulesByZone(
+              budget.modules
+            ),
+            total: this.budgetService.calculateTotal(budget.modules),
+          }))
+        );
+      })
+    );
+  }
+
+  getZones(groupedModules: GroupedModules): string[] {
+    return Object.keys(groupedModules);
+  }
 }
